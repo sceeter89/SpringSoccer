69eed9c472ce909309a43b6d9972156354c5c149
Yakuza <sceeter89@outlook.com>
2017-07-15 23:12:29 +0200

    Improved pawn

--- SpringSoccer/Assets/Scripts/PawnController.cs
+++ SpringSoccer/Assets/Scripts/PawnController.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class PawnController : MonoBehaviour
 {
 	private Rigidbody _rigidBody;
-	private MeshFilter _meshFilter;
+	private bool _slowingDown;
 
 	public GameObject forceApplicationPoint;
-	public float currentRotation = 0f;
+	public Vector3 currentSpeed;
 	public float force = 100f;
 
 	void Start ()
 	{
 		_rigidBody = GetComponent<Rigidbody> ();
-		_meshFilter = GetComponent<MeshFilter> ();
+	}
+
+	void Update()
+	{
+		currentSpeed = _rigidBody.velocity;
+
+		if (_slowingDown) {
+			_rigidBody.AddForceAtPosition(currentSpeed * -0.7f, forceApplicationPoint.transform.position, ForceMode.Acceleration);
+		}
+
 	}
 
 	void OnMouseDown ()
 	{
 		//_rigidBody.transform.RotateAround (rotationPivot.transform.position, Vector3.right, 45);
 
-		//_rigidBody.AddForceAtPosition(Vector3.left * force, forceApplicationPoint.transform.position, ForceMode.Acceleration);
-		_rigidBody.AddForce(Vector3.left * force, ForceMode.Acceleration);
+		_rigidBody.AddForceAtPosition(Vector3.left * force, forceApplicationPoint.transform.position, ForceMode.Acceleration);
+		_slowingDown = true;
+		//_rigidBody.AddForce(Vector3.left * force, ForceMode.Acceleration);
 		//_rigidBody.useGravity = true;
 	}
 }
